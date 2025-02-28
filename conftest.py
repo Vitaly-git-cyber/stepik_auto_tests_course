5814f887bb4ece8f1da6f17b64068ee1c2bb9d56
Vitaly <saimon7_90@mail.ru>
2025-02-28 13:32:22 +0300

    Добавление поддержки языка для FireFox

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -20,7 +21,9 @@ def browser(request):
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages",language)
+        browser = webdriver.Firefox(options=options_firefox)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
